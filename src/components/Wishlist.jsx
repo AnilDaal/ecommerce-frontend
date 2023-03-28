1343c03318f12759ccb7fd2c52c894d4a25090cd
AnilDaal <anildaal7@gmail.com>
2023-03-28 16:00:39 +0530

    bug fix

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,118 +1,125 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import Button from "react-bootstrap/Button";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import {
-  decrementwishlist,
-  incrementwishlist,
-  resetwishlist,
-  removewishlist,
-  getTotals,
-} from "../store";
-import PayButton from "./stripe/PayButton";
-import "./wishlist.css";
+// /* eslint-disable react-hooks/rules-of-hooks */
+// import Button from "react-bootstrap/Button";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Link } from "react-router-dom";
+// import {
+//   decrementwishlist,
+//   incrementwishlist,
+//   resetwishlist,
+//   removewishlist,
+//   getTotals,
+// } from "../store";
+// import PayButton from "./stripe/PayButton";
+// import "./wishlist.css";
 
-const wishlist = () => {
-  const { wishlistItems, wishlistTotalAmount } = useSelector(
-    (state) => state.wishlist
-  );
-  const { token } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-  const handleReset = () => {
-    dispatch(resetwishlist());
-  };
-  const handleIncrementwishlist = (product) => {
-    dispatch(incrementwishlist(product));
-  };
-  const handleDecrementwishlist = (product) => {
-    dispatch(decrementwishlist(product));
-  };
-  const handlewishlistRemove = (product) => {
-    dispatch(removewishlist(product));
-  };
+// const wishlist = () => {
+//   const { wishlistItems, wishlistTotalAmount } = useSelector(
+//     (state) => state.wishlist
+//   );
+//   const { token } = useSelector((state) => state.auth);
+//   const dispatch = useDispatch();
+//   const handleReset = () => {
+//     dispatch(resetwishlist());
+//   };
+//   const handleIncrementwishlist = (product) => {
+//     dispatch(incrementwishlist(product));
+//   };
+//   const handleDecrementwishlist = (product) => {
+//     dispatch(decrementwishlist(product));
+//   };
+//   const handlewishlistRemove = (product) => {
+//     dispatch(removewishlist(product));
+//   };
 
-  // dispatch(getTotals());
-  // useEffect(() => {
-  //   dispatch(getTotals());
-  // }, [dispatch]);
+//   // dispatch(getTotals());
+//   // useEffect(() => {
+//   //   dispatch(getTotals());
+//   // }, [dispatch]);
 
-  dispatch(getTotals());
-  return (
-    <div className="container">
-      <h1>Shopping wishlist</h1>
-      {wishlistItems.length === 0 && (
-        <p>
-          Your wishlist is empty click <Link to="/">Here</Link>
-        </p>
-      )}
-      <div>
-        <Link to="/">&#x2190; Continue Shopping</Link>
-      </div>
-      <div>
-        {wishlistItems.map((product) => (
-          <div key={product.ID} className="wishlist_single">
-            <img src={product.Images} alt={product.Name} />
-            <p className="wishlist-title">{product.Name}</p>
-            <p>&#8377;{product["Regular price"]}</p>
-            <button
-              className="btn btn-danger"
-              onClick={() => handlewishlistRemove(product)}
-            >
-              Remove
-            </button>
-            <div>
-              <button onClick={() => handleDecrementwishlist(product)}>
-                -
-              </button>
-              <span>{product.wishlistQuantity}</span>
-              <button onClick={() => handleIncrementwishlist(product)}>
-                +
-              </button>
-            </div>
-            <h6 style={{ paddingRight: "1rem" }}>
-              total:&#8377;
-              {(product["Regular price"] * product.wishlistQuantity).toFixed(2)}
-            </h6>
-          </div>
-        ))}
+//   dispatch(getTotals());
+//   return (
+//     <div className="container">
+//       <h1>Shopping wishlist</h1>
+//       {wishlistItems.length === 0 && (
+//         <p>
+//           Your wishlist is empty click <Link to="/">Here</Link>
+//         </p>
+//       )}
+//       <div>
+//         <Link to="/">&#x2190; Continue Shopping</Link>
+//       </div>
+//       <div>
+//         {wishlistItems.map((product) => (
+//           <div key={product.ID} className="wishlist_single">
+//             <img src={product.Images} alt={product.Name} />
+//             <p className="wishlist-title">{product.Name}</p>
+//             <p>&#8377;{product["Regular price"]}</p>
+//             <button
+//               className="btn btn-danger"
+//               onClick={() => handlewishlistRemove(product)}
+//             >
+//               Remove
+//             </button>
+//             <div>
+//               <button onClick={() => handleDecrementwishlist(product)}>
+//                 -
+//               </button>
+//               <span>{product.wishlistQuantity}</span>
+//               <button onClick={() => handleIncrementwishlist(product)}>
+//                 +
+//               </button>
+//             </div>
+//             <h6 style={{ paddingRight: "1rem" }}>
+//               total:&#8377;
+//               {(product["Regular price"] * product.wishlistQuantity).toFixed(2)}
+//             </h6>
+//           </div>
+//         ))}
 
-        {wishlistItems.length > 0 && (
-          <>
-            <div className="wishlist-total">
-              <button onClick={handleReset}>Clear wishlist</button>
-              <div>
-                <span>Subtotal</span>
-                <strong>&#8377;{wishlistTotalAmount.toFixed(2)}</strong>
-              </div>
-            </div>
-          </>
-        )}
+//         {wishlistItems.length > 0 && (
+//           <>
+//             <div className="wishlist-total">
+//               <button onClick={handleReset}>Clear wishlist</button>
+//               <div>
+//                 <span>Subtotal</span>
+//                 <strong>&#8377;{wishlistTotalAmount.toFixed(2)}</strong>
+//               </div>
+//             </div>
+//           </>
+//         )}
 
-        {wishlistItems.length > 0 && (
-          <div style={{ textAlign: "end" }}>
-            <p>Taxes and shipping charge calculated at checkout page</p>
-            {token ? (
-              <PayButton wishlistItems={wishlistItems} />
-            ) : (
-              <Button variant="warning">
-                <Link
-                  to="/login"
-                  style={{ color: "white", fontWeight: "bold" }}
-                  className="imp-link"
-                >
-                  Login To Checkout
-                </Link>
-              </Button>
-            )}
-          </div>
-        )}
-      </div>
-    </div>
-  );
-};
+//         {wishlistItems.length > 0 && (
+//           <div style={{ textAlign: "end" }}>
+//             <p>Taxes and shipping charge calculated at checkout page</p>
+//             {token ? (
+//               <PayButton wishlistItems={wishlistItems} />
+//             ) : (
+//               <Button variant="warning">
+//                 <Link
+//                   to="/login"
+//                   style={{ color: "white", fontWeight: "bold" }}
+//                   className="imp-link"
+//                 >
+//                   Login To Checkout
+//                 </Link>
+//               </Button>
+//             )}
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
 
-const Wishlist = () => {
-  return <div>{wishlist}</div>;
-};
+// const Wishlist = () => {
+//   return <div>{wishlist}</div>;
+// };
+
+// export default Wishlist;
+import React from "react";
+
+function Wishlist() {
+  return <div>Wishlist</div>;
+}
 
 export default Wishlist;
--- src/components/products/products/Products.jsx
+++ src/components/products/products/Products.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { arr } from '../../../data/jsonTestData';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { arr } from "../../../data/jsonTestData";
 
-import { addToCart } from '../../../store';
-import './products.css';
+import { addToCart } from "../../../store";
+import "./products.css";
 
 const Products = () => {
   const dispatch = useDispatch();
@@ -13,8 +14,8 @@ const Products = () => {
     dispatch(addToCart(product));
   };
   return (
-    <section class="section product" style={{ paddingBottom: '60px' }}>
-      <div class="container" style={{ margin: '0 auto' }}>
+    <section class="section product" style={{ paddingBottom: "60px" }}>
+      <div class="container" style={{ margin: "0 auto" }}>
         <h2 class="h2 section-title">Products of the week</h2>
 
         {/* <ul class="filter-list">
@@ -40,67 +41,37 @@ const Products = () => {
           <h2 class="text-2xl font-medium text-gray-800 uppercase mb-6">
             recomended for you
           </h2>
-          <div class="grid grid-cols-5 gap-6">
+          <div class="box-flex">
             {arr.slice(0, 10).map((item) => {
               return (
-                <div class="group rounded bg-white shadow overflow-hidden">
-                  <div class="relative">
-                    <img src={item.Images} class="w-full" alt={item.Name} />
-                    <div class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
-                      <a
-                        href="view.html"
-                        class="text-white text-lg w-9 h-9 rounded-full bg-primary hover:bg-gray-800 transition flex items-center justify-center"
-                      >
-                        <i class="fas fa-search"></i>
-                      </a>
-                      <a
-                        href="#"
-                        class="text-white text-lg w-9 h-9 rounded-full bg-primary hover:bg-gray-800 transition flex items-center justify-center"
-                      >
-                        <i class="far fa-heart"></i>
-                      </a>
-                    </div>
+                <div class="box-flex-middle">
+                  <div>
+                    <img src={item.Images} alt={item.Name} />
                   </div>
-
-                  <div class="pt-4 pb-3 px-4">
+                  <div>
                     <a href="view.html">
                       <h4 class="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition">
                         {item.Name}
                       </h4>
                     </a>
-                    <div class="flex items-baseline mb-1 space-x-2">
-                      <p class="text-xl text-primary font-roboto font-semibold">
-                        &#8377;{item['Regular price']}
-                      </p>
-                      <p class="text-sm text-gray-400 font-roboto line-through">
-                        $55.00
-                      </p>
+                    <div class=" items-baseline mb-1 space-x-2">
+                      <p>&#8377;{item["Regular price"]}</p>
                     </div>
-                    <div class="flex items-center">
-                      <div class="flex gap-1 text-sm text-yellow-400">
-                        <span>
-                          <i class="fas fa-star"></i>
-                        </span>
-                        <span>
-                          <i class="fas fa-star"></i>
-                        </span>
-                        <span>
-                          <i class="fas fa-star"></i>
-                        </span>
-                        <span>
-                          <i class="fas fa-star"></i>
-                        </span>
-                        <span>
-                          <i class="fas fa-star"></i>
-                        </span>
+                    <div class="start-chage-size">
+                      <div>
+                        <span>⭐</span>
+                        <span>⭐</span>
+                        <span>⭐</span>
+                        <span>⭐</span>
+                        <span>⭐</span>
                       </div>
-                      <div class="text-xs text-gray-500 ml-3">(150)</div>
+                      <p>(150)</p>
                     </div>
                   </div>
 
                   <a
                     href="#"
-                    class="block w-full py-1 text-center text-white bg-primary border border-primary rounded-b hover:bg-transparent hover:text-primary transition"
+                    class="block w-full py-1 text-center text-white bg-primary border border-primary rounded-b hover:bg-transparent hover:text-primary transition "
                   >
                     Add to Cart
                   </a>
