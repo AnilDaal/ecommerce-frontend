f6e7a2e96096c7613f2829431498f703f66daccb
AnilDaal <anildaal7@gmail.com>
2023-03-30 12:33:30 +0530

    make responsive nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -38,10 +38,10 @@ const Navbar = () => {
 
   return (
     <header class="text-gray-600 body-font ">
-      <div class="container mx-auto flex  p-5 flex-col md:flex-row items-center justify-between">
+      <div class=" container mx-auto nav-res">
         <Link
           to="/"
-          class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+          class="flex title-font font-medium items-center text-gray-900 md:mb-0"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -91,44 +91,65 @@ const Navbar = () => {
             </nav>
           </>
         )} */}
-        <div className="flex items-center">
-          {!token && (
-            <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center gap-5">
-              <Link
-                class="mr-5 hover:text-gray-900"
-                className="header-action-btn"
-                to="/cart"
+        <div className="nav-res-show">
+          <div>
+            {!token && (
+              <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 nav-res-icon">
+                <Link
+                  class="mr-5 hover:text-gray-900"
+                  className="header-action-btn"
+                  to="/cart"
+                >
+                  {/* <p className="header-action-label">Cart</p> */}
+                  <div className="btn-badge bg-teal-600" aria-hidden="true">
+                    {cartTotalQuantity}
+                  </div>
+                  <BsCart2 className="text-2xl" />
+                </Link>
+                <Link
+                  class="mr-5 hover:text-gray-900"
+                  className="header-action-btn"
+                >
+                  <AiOutlineHeart className="text-2xl" />
+
+                  {/* <p className="header-action-label">Wish</p> */}
+
+                  <div className="btn-badge bg-teal-600" aria-hidden="true">
+                    0
+                  </div>
+                </Link>
+                <Link class="mr-5 hover:text-gray-900" to="/product-list">
+                  <BsBag className="text-2xl" />
+                </Link>
+
+                {/* <Link class="mr-5 hover:text-gray-900">Fourth Link</Link> */}
+              </nav>
+            )}
+          </div>
+          <div>
+            {!token ? (
+              <Link to="/customer-login">
+                <button class="inline-flex items-center bg-teal-400 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base  md:mt-0">
+                  Login
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    class="w-4 h-4 ml-1"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                  </svg>
+                </button>
+              </Link>
+            ) : (
+              <button
+                onClick={() => dispatch(logoutUser())}
+                class="inline-flex items-center bg-red-400 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
               >
-                {/* <p className="header-action-label">Cart</p> */}
-                <div className="btn-badge bg-teal-600" aria-hidden="true">
-                  {cartTotalQuantity}
-                </div>
-                <BsCart2 className="text-2xl" />
-              </Link>
-              <Link
-                class="mr-5 hover:text-gray-900"
-                className="header-action-btn"
-              >
-                <AiOutlineHeart className="text-2xl" />
-
-                {/* <p className="header-action-label">Wish</p> */}
-
-                <div className="btn-badge bg-teal-600" aria-hidden="true">
-                  0
-                </div>
-              </Link>
-
-              <Link class="mr-5 hover:text-gray-900" to="/product-list">
-                <BsBag className="text-2xl" />
-              </Link>
-
-              {/* <Link class="mr-5 hover:text-gray-900">Fourth Link</Link> */}
-            </nav>
-          )}
-          {!token ? (
-            <Link to="/customer-login">
-              <button class="inline-flex items-center bg-teal-400 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-                Login
+                Logout
                 <svg
                   fill="none"
                   stroke="currentColor"
@@ -141,26 +162,8 @@ const Navbar = () => {
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
               </button>
-            </Link>
-          ) : (
-            <button
-              onClick={() => dispatch(logoutUser())}
-              class="inline-flex items-center bg-red-400 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
-            >
-              Logout
-              <svg
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                class="w-4 h-4 ml-1"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-              </svg>
-            </button>
-          )}
+            )}
+          </div>
         </div>
       </div>
     </header>
