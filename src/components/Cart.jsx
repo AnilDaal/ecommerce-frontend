e30476f35f013ea2baebb62b66225f82ae6efa67
deepak1051 <lightyagamigoku@gmail.com>
2023-04-10 14:21:10 +0530

    cart page styling

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getTotals } from '../store';
-
+import { AiFillDelete } from 'react-icons/ai';
 import './cart.css';
 import { useEffect } from 'react';
 import {
@@ -88,58 +88,89 @@ const Cart = () => {
       </button>
 
       {productCartItems?.length > 0 ? (
-        <div className="py-10">
-          {productCartItems?.map(({ productId: product, productQuantity }) => {
-            console.log(product);
-            return (
-              <div key={product._id} className="cart_single">
-                <img src={product.image} alt={product.title} />
-                <p className="cart-title">{product.title}</p>
-                <p>&#8377;{product.price}</p>
+        <div className="py-10 cart_single-wrapper mx-6 flex gap-2 ">
+          <div className="d-con-1">
+            {productCartItems?.map(
+              ({ productId: product, productQuantity }) => {
+                console.log(product);
+                return (
+                  <div key={product._id} className="cart_single ">
+                    <div className="flex flex-col items-center gap-2 justify-center  w-48">
+                      <img
+                        src={product.image}
+                        alt={product.title}
+                        style={{
+                          width: '100%',
+                          height: '100px',
+                          objectFit: 'contain',
+                          borderRadius: '10px',
+                        }}
+                      />
+                      <div style={{ display: 'flex', gap: '4px' }}>
+                        <button
+                          disabled={productQuantity < 2}
+                          onClick={() =>
+                            handleDecrementCart({
+                              productQuantity,
+                              productId: product._id,
+                            })
+                          }
+                          className="border px-2 "
+                          style={{ fontSize: '1.2rem' }}
+                        >
+                          -
+                        </button>
+                        <span>{productQuantity}</span>
+                        <button
+                          // disabled={productQuantity === product.totalQuantity}
+                          className="border px-2 "
+                          style={{ fontSize: '1.2rem' }}
+                          onClick={() =>
+                            handleIncrementCart({
+                              productQuantity,
+                              productId: product._id,
+                              totalQuantity: product.totalQuantity,
+                            })
+                          }
+                        >
+                          +
+                        </button>
+                      </div>
+                    </div>
 
-                <div style={{ display: 'flex', gap: '4px' }}>
-                  <button
-                    disabled={productQuantity < 2}
-                    onClick={() =>
-                      handleDecrementCart({
-                        productQuantity,
-                        productId: product._id,
-                      })
-                    }
-                    className="border px-2 "
-                    style={{ fontSize: '1.2rem' }}
-                  >
-                    -
-                  </button>
-                  <span>{productQuantity}</span>
-                  <button
-                    // disabled={productQuantity === product.totalQuantity}
-                    className="border px-2 "
-                    style={{ fontSize: '1.2rem' }}
-                    onClick={() =>
-                      handleIncrementCart({
-                        productQuantity,
-                        productId: product._id,
-                        totalQuantity: product.totalQuantity,
-                      })
-                    }
-                  >
-                    +
-                  </button>
-                </div>
-                <h6 style={{ paddingRight: '1rem' }}>
-                  total:&#8377;
-                  {(product.price * productQuantity).toFixed(2)}
-                </h6>
-                <button
-                  className="btn btn-danger bg-red-600 border-none"
-                  onClick={() => handleCartRemove(product)}
-                >
-                  Remove
-                </button>
-              </div>
-            );
-          })}
+                    <div className="flex gap-24 d-gap-small">
+                      <div className="flex flex-col gap-2">
+                        <p className="cart-title text-sm text-gray-400">
+                          {product.title}
+                        </p>
+                        <p className="text-xl text-gray-800 font-bold">
+                          &#8377;{product.price}
+                        </p>
+                      </div>
+                      <div className="flex flex-col gap-2">
+                        <h6
+                          style={{ paddingRight: '1rem' }}
+                          className="font-bold text"
+                        >
+                          total:&#8377;
+                          {(product.price * productQuantity).toFixed(2)}
+                        </h6>
+                        <button
+                          className="text-2xl text-red-600"
+                          onClick={() => handleCartRemove(product)}
+                        >
+                          <AiFillDelete />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                );
+              }
+            )}
+          </div>
+          <div className="d-con-2">
+            <button className="btn bg-yellow-300">Checkout</button>
+          </div>
         </div>
       ) : (
         <div className="  font-bold text-center py-6">
